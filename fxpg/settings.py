3e9724ec4728f0851e990d1bc2ce2f17b19e1b5f
plugrapher <147877101+plugrapher@users.noreply.github.com>
2024-05-09 00:18:11 +0300

    Update settings.py

--- fxpg/settings.py
+++ fxpg/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2!nkux=iaf+o0kbmly3gnyz=5xju-y*_%28y64pxn3%oh6#10y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
